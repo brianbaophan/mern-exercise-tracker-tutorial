1a74380a4134eb568141fcaf3a475f01459e2a7e
brianxphan <brianxphan@gmail.com>
2020-03-06 19:17:24 -0800

    1. Connected to DB (done) 2. Backend (done) 3. front-end (in progress) - wrapping up create exercise component

---
+++ src/components/create-user.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateUsers extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Create Users component!</p>
+      </div>
+    );
+  }
+}
+
+export default CreateUsers;
