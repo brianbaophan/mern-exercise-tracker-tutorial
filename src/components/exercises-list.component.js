abcf33c57c30d4f7e731cebd6a26449eb8cca62f
brianxphan <brianxphan@gmail.com>
2020-03-10 18:15:17 -0700

    fixed landing page bug and added delete and mount functions in exercise list

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class ExercisesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+
+    this.state = {
+      exercises: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/exercises')
+      .then(res => this.setState({ exercises: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  deleteExercise(id) {
+    axios
+      .delete(`http://localhost:5000/exercises/${id}`)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      exercises: this.state.exercises.filter(exercise => exercise._id !== id)
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>You are on the Exercises List component!</p>
+        <h3>Logged Exercises</h3>
       </div>
     );
   }
