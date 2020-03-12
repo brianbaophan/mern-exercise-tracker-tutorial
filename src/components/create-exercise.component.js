c11a691314b2b52c92a8cb27226b6496a5abe070
brianxphan <brianxphan@gmail.com>
2020-03-11 18:33:41 -0700

    finished exercise app

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -71,7 +71,7 @@ class CreateExercises extends Component {
       .post('http://localhost:5000/exercises/add', exercise)
       .then(res => console.log(res.data));
 
-    // window.location = '/';
+    window.location = '/';
   }
 
   render() {
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+function Exercise(props) {
+  return (
+    <tr>
+      <td>{props.exercise.username}</td>
+      <td>{props.exercise.description}</td>
+      <td>{props.exercise.duration}</td>
+      <td>{props.exercise.date.substring(0, 10)}</td>
+      <td>
+        <Link to={`/edit/${props.exercise._id}`}>edit</Link> |{' '}
+        <a
+          href="#"
+          onClick={() => {
+            props.deleteExercise(props.exercise._id);
+          }}
+        >
+          delete
+        </a>
+      </td>
+    </tr>
+  );
+}
+
 class ExercisesList extends Component {
   constructor(props) {
     super(props);
@@ -30,10 +52,34 @@ class ExercisesList extends Component {
     });
   }
 
+  exerciseList() {
+    return this.state.exercises.map(exercise => {
+      return (
+        <Exercise
+          exercise={exercise}
+          deleteExercise={this.deleteExercise}
+          key={exercise._id}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.exerciseList()}</tbody>
+        </table>
       </div>
     );
   }
